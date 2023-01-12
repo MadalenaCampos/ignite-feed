2a44bec4a708f1e01a059d2a856ef063605d64eb
Madalena Campos <madacampos02@gmail.com>
2023-01-12 11:23:49 -0300

    criando o sidebar

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,35 @@
+import "./global.css";
+import style from "./App.module.css";
+
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 import { Post } from "./Post";
 
-import "./global.css";
-
 export function App() {
-  return <Header />;
+  return (
+    <div className="ignite-feed">
+      <div className="ignite-feed-header">
+        <Header />
+      </div>
+
+      <div className={style["ignite-feed-timeline"]}>
+        <Sidebar />
+
+        <div className="ignite-feed-timeline-posts">
+          <Post
+            author="Joana"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas laborum iste aspernatur, excepturi illum quisquam non ipsum sequi eveniet aperiam, iusto itaque vel veniam maiores laudantium quae deserunt, voluptatum veritatis."
+          />
+          <Post
+            author="Mauriciu"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas laborum iste aspernatur, excepturi illum quisquam non ipsum sequi eveniet aperiam, iusto itaque vel veniam maiores laudantium quae deserunt, voluptatum veritatis."
+          />
+          <Post
+            author="Bella"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas laborum iste aspernatur, excepturi illum quisquam non ipsum sequi eveniet aperiam, iusto itaque vel veniam maiores laudantium quae deserunt, voluptatum veritatis."
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -0,0 +1,5 @@
+import style from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return <p>Sidebar</p>;
+}
