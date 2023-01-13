776bfd7994426af85b4b287c7f4a7e4255e42cd4
Madalena Campos <madacampos02@gmail.com>
2023-01-13 14:00:54 -0300

    concluindo o sidebar

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,5 +1,25 @@
 import style from "./Sidebar.module.css";
 
+// Ícones do Phosphor
+import { PencilLine } from "phosphor-react";
+
 export function Sidebar() {
-  return <p>Sidebar</p>;
+  return (
+    <div className={style.sidebar}>
+      <img src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" />
+
+      <div className={style["sidebar-perfil"]}>
+        <img src="https://avatars.githubusercontent.com/u/71613655?v=4" />
+
+        <h4>Madalena Campos</h4>
+        <p>Desenvolvedora Front-end</p>
+      </div>
+
+      <div className={style["sidebar-editar-perfil"]}>
+        <button type="button">
+          <PencilLine size={20}/> Editar seu perfil
+        </button>
+      </div>
+    </div>
+  );
 }
